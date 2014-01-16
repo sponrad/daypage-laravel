7a947fad0d6b6bbe4d290cc015a33b1b2b70df71
sponrad <sponrad@gmail.com>
2014-01-16 10:11:22 -0800

    remove saving title on entry

--- app/controllers/JsonController.php
+++ app/controllers/JsonController.php
@@ -25,7 +25,7 @@ class JsonController extends BaseController {
     $entry->user()->associate($user);
     $entry->date = Input::get('date'); 
     $entry->content = Input::get('content');
-    $entry->title = substr(explode("\n", Input::get('content'))[0], 0, 100);    
+//    $entry->title = substr(explode("\n", Input::get('content'))[0], 0, 100);    
     $entry->save();
 
     return Response::json( array('response' => 1, 'entryId' => $entry->id) );
