25976ef7e72bd0ba91e19cb8cd71ee08c8da5850
sponrad <sponrad@gmail.com>
2014-01-16 11:53:19 -0800

    more content

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -1,6 +1,21 @@
 @extends('main')
 
 @section('content')
-  About
+  <h2>Who we are</h2>
+  <h3>Conrad</h3>
+  <p>Conrad is the developer</p>
+  <h3>Tess</h3>
+  <p>Tess is the artist and designer</p>
+  <p>We are Devlabtech, a small private business in San Diego, CA</p>
+
+  <h2>Technology Credits</h2>
+  <p>DigitalOcean - VPS hosting</p>
+  <p>Ubuntu - Operating system</p>
+  <p>Apache - Server</p>
+  <p>MySQL - Database</p>
+  <p>Laravel - PHP web framework</p>
+  <p>Jquery - Javascript help</p>
+  <p>Twitter Bootstrap - CSS framework</p>
+  
   <img src="/img/usedaypage3.jpg" />
 @stop
--- app/views/features.blade.php
+++ app/views/features.blade.php
@@ -1,6 +1,19 @@
 @extends('main')
 
 @section('content')
-  Features
+  <h2>Features</h2>
+  <h3>Notes organized by day</h3>
+  <h3>Edit entries on todays date, or in the past and the future</h3>
+  <h3>Unique horizontal date navigation browser</h3>
+  <h3>Searchable</h3>
+  <h3>Super Secure</h3>
+  <h3>No autosave</h3>
+  <h3>No ads</h3>
+  <h3>Fantastic editor to type in</h3>
+  <h3>See group members notes</h3>
+  <h3>Multiple entries per day</h3>
+  <h3>Optional passwords on personal entries</h3>
+  <h3>Reminders to write</h3>
+  <h3>Easy backup</h3>
   <img src="/img/shades.jpg" />
 @stop
